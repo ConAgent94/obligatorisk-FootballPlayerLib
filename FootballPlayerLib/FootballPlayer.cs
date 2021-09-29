27cb57935b98981171f758c5d70ac7e611f858fc
Unknown <Unknown@DESKTOP-JAUNVAN>
2021-09-29 12:12:23 +0200

    Add project files.

---
+++ FootballPlayerLib/FootballPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FootballPlayerLib
+{
+    public class FootballPlayer
+    {
+        private string _name;
+        private int _price;
+        private int _shirtno;
+
+        public FootballPlayer(int id, string name, int price, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+
+
+        public int Id { get; set;}
+
+
+        public string Name { get => _name;
+
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "Name is null");
+                if (value.Length < 4) throw new ArgumentException("Name length must be at least 4 characters");
+                _name = value;
+            }
+
+        }
+
+        public int Price { get => _price;
+            set 
+            {
+                if (value == 0) throw new ArgumentNullException(" Price is not allowed to be 0");
+                _price = value;
+            }
+        }
+
+
+        public int ShirtNumber { get=> _shirtno;
+            set 
+            {
+                if (1 <= value && value <= 100) _shirtno = value;
+                else throw new ArgumentOutOfRangeException("Shirtno:",value, "Not allowed. Only 1-100");
+            } 
+        }
+
+    }
+}
---
+++ FootballPlayerLibTests1/FootballPlayerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootballPlayerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballPlayerLib.Tests
+{
+
+    
+
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+
+        private FootballPlayer _footballPlayer;
+
+        [TestInitialize]
+        public void Start()
+        {
+            _footballPlayer = new FootballPlayer(1, "Peter", 100, 1);
+        }
+
+
+        [TestMethod()]
+        public void ConsTest()
+        {
+            FootballPlayer footballPlayer = new FootballPlayer(1, "Peter", 100, 1);
+            Assert.AreEqual("Peter", footballPlayer.Name);
+            Assert.AreEqual(1, footballPlayer.Id);
+            Assert.AreEqual(100, footballPlayer.Price);
+            Assert.AreEqual(1, footballPlayer.ShirtNumber);
+
+        }
+
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            _footballPlayer.Name = "Peter";
+            Assert.AreEqual("Peter", _footballPlayer.Name);
+
+
+            Assert.ThrowsException<ArgumentNullException>(() => _footballPlayer.Name = null);
+            Assert.ThrowsException<ArgumentException>(() => _footballPlayer.Name = "");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => _footballPlayer.Price = 0);
+
+            _footballPlayer.Price = 100;
+            Assert.AreEqual(100, _footballPlayer.Price);
+        }
+
+        [TestMethod()]
+        public void ShirtTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.ShirtNumber = 0);
+
+            _footballPlayer.ShirtNumber = 1;
+            Assert.AreEqual(1, _footballPlayer.ShirtNumber);
+
+            _footballPlayer.ShirtNumber = 100;
+            Assert.AreEqual(100, _footballPlayer.ShirtNumber);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.ShirtNumber = 101);
+        }
+
+    }
+}
